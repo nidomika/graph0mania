d8c092c01c2d00afe5a6078904592be67873c21a
Dominika Szymanska <dominika1872@gmail.com>
2020-01-30 23:33:01 +0100

    this will be serving different purpose

--- plot.js
+++ plot.js
@@ -1,70 +1,22 @@
-function handleChange(target) {
-    const reader = new FileReader();
-    reader.onload = handleReaderLoad;
-    reader.readAsText(target.files[0]);
-}
+const ctx = document.getElementById('myChart').getContext('2d');
+const xlabels = [];
 
-function handleReaderLoad(event) {
-    const parsedObject = JSON.parse(event.target.result);
-    console.log('parsed object', parsedObject);
-    const output = parsedObject.participants.map(({
-        name
-    }) => ({
-        name,
-        counts: {},
-    }))
-    parsedObject.messages.forEach(message => {
-        // Find the correct participant in the output object
-        const outputParticipant = output.find(({
-            name
-        }) => name === message.sender_name)
-
-        // Increment the reaction counts for that participant
-        message.reactions.forEach(({
-            reaction
-        }) => {
-            if (!outputParticipant.counts[reaction]) {
-                outputParticipant.counts[reaction] = 1
-            } else {
-                outputParticipant.counts[reaction] += 1
-            }
-        })
-    })
-}
-// parsedObject.messages.forEach(message => {
-//     // Find the correct participant in the output object
-//     const outputParticipant = output.find(({
-//         name
-//     }) => name === message.sender_name)
-
-//     // Increment the reaction counts for that participant
-//     message.reactions.forEach(({
-//         reaction
-//     }) => {
-//         if (!outputParticipant.counts[reaction]) {
-//             outputParticipant.counts[reaction] = 1
-//         } else {
-//             outputParticipant.counts[reaction] += 1
-//         }
-//     })
-// })
-
-// const output = parsedObject.participants.map(({
-//     name
-// }) => ({
-//     name,
-//     counts: {},
-// }))
-// const output = parsedObject.participants.map(({
-//     name
-// }) => ({
-//     name,
-//     counts: {},
-// }))
-
-function timestampConverter(timestamp) {
-    const time = new Date(timestamp);
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    let i = time.getMonth();
-    console.log(months[i], time.getFullYear());
-}
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: xlabels, //tu bym chciala daty
+        datasets: [{
+            label: 'number of messages between you and your partner in crime over time',
+            data: [12, 19, 3, 5, 2, 3], //no a tu bedzie liczba wiadomosci ???
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
