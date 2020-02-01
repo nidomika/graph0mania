7e03998a1048c4f66cd6de75263c68255311e694
Dominika Szymanska <dominika1872@gmail.com>
2020-02-01 04:22:32 +0100

    changed... nothing really

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+function handleChange(target) {
+    const reader = new FileReader();
+    reader.onload = function handleReaderLoad(event) {
+        const parsedObject = JSON.parse(event.target.result);
+        console.log('jeb panie oto parsedObject', parsedObject);
+    }
+    reader.readAsText(target.files[0]);
+}
+
+function timestampConverter(timestamp) {
+    const time = new Date(timestamp);
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let i = time.getMonth();
+    console.log(months[i], time.getFullYear());
+}
+
+//potem se naprawię
 // const imput = document.querySelector('input[type="file"]')
 // imput.addEventListener('change', function read(event) {
 //     // console.log(imput.files);
@@ -8,22 +25,4 @@
 //         callback(reader.result);
 //     }
 //     console.log(dupa);
-// }, false); //co tu sie to ja nawet nie
-
-function handleChange(target) { //tu cos wykrzacza
-    const reader = new FileReader();
-    reader.onload = handleReaderLoad;
-    reader.readAsText(target.files[0]);
-}
-
-function handleReaderLoad(event) {
-    const parsedObject = JSON.parse(event.target.result);
-    console.log('parsed object', parsedObject);
-}
-
-function timestampConverter(timestamp) {
-    const time = new Date(timestamp);
-    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    let i = time.getMonth();
-    console.log(months[i], time.getFullYear());
-}
+// }, false); //co tu sie to ja nawet nie (już chyba wiem co tu się)
