05e909f4249a4955241ec85927126f4b1702ca19
Dominika Szymanska <dominika1872@gmail.com>
2020-02-04 15:51:31 +0100

    added missing semicolons and comments

--- main.js
+++ main.js
@@ -10,34 +10,34 @@ function handleChange(target) {
         const parsedObject = JSON.parse(fixFbJson(event.target.result));
         yourPartner = parsedObject.participants[0].name;
         let partners = parsedObject.participants.length;
-        let groupName = parsedObject.title;
+        let groupName = parsedObject.title; //for 2 people convos, yourPartner = groupName
         if (partners > 2) {
             yourPartner = partners + " people";
             document.getElementById('groupName').textContent = "your group name is " + groupName;
         } else document.getElementById('groupName').textContent = "";
         document.getElementById('messagingPartner').textContent = "it seems like you've been messaging with " + yourPartner;
         parsedObject.messages.forEach((message, index) => {
-            messagingData.x.push(dayjs(message.timestamp_ms).format('DD-MM-YYYY'));
+            messagingData.x.push(moment(message.timestamp_ms).format('DD-MM-YYYY'));
             messagingData.y.push(index + 1);
         });
         // console.log(messagingData);
-        console.log(getPoints(messagingData));
+        // console.log(getPoints(messagingData));
     }
     reader.readAsBinaryString(target.files[0]);
 }
 
 function fixFbJson(json) {
-    const textDecoder = new TextDecoder()
+    const textDecoder = new TextDecoder();
     return json.replace(/\\u00(..)\\u00(..)/g, (...args) => {
-        const p1 = parseInt(args[1], 16)
-        const p2 = parseInt(args[2], 16)
-        return textDecoder.decode(new Int8Array([p1, p2]))
-    })
+        const p1 = parseInt(args[1], 16);
+        const p2 = parseInt(args[2], 16);
+        return textDecoder.decode(new Int8Array([p1, p2]));
+    });
 }
 
 function isUploaded() {
     if (document.getElementById('giveData').value !== "") {
-        console.log("bravo, file uploaded");
+        console.log("bravo, file uploaded succsessfully");
         location.href = '#graphMe';
     } else {
         window.alert("you didn't actually upload any file, did you?");
@@ -49,6 +49,6 @@ function getPoints(array) {
     const x = array.x;
     const y = array.y;
     const result = {};
-    x.forEach((x, i) => result[x] = y[i])
+    x.forEach((x, i) => result[x] = y[i]);
     return result;
 }
