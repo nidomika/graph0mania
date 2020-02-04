6ea46a827daa68863eb2a9f211572f3a713aab6d
Dominika Szymanska <dominika1872@gmail.com>
2020-02-04 03:24:30 +0100

    under construction

--- plot.js
+++ plot.js
@@ -1,22 +1,23 @@
 const ctx = document.getElementById('myChart').getContext('2d');
-const xlabels = [];
 
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: xlabels, //tu bym chciala daty
         datasets: [{
             label: 'number of messages between you and your partner in crime over time',
-            data: [12, 19, 3, 5, 2, 3], //no a tu bedzie liczba wiadomosci ???
+            data: messagingData.x,
+            fill: false,
         }]
     },
     options: {
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
+            xAxes: [{
+                type: 'time',
+                time: {
+                    unit: 'day'
                 }
             }]
         }
+
     }
 });
